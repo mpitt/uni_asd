0eb8c4a2ed64a3d63f4df76c7aa7a2b035721268
Michele Pittoni <michele@pittoni.org>
2013-03-13 21:46:43 +0100

    [intervalli] Correct algorithm and implementation
    - Add a Makefile
    - Remove binary and output file

--- lab02/intervalli/intervalli.cpp
+++ lab02/intervalli/intervalli.cpp
@@ -1,8 +1,10 @@
 #include <fstream>
-#include <utility>
 #include <algorithm>
 
-pair<int,int> A[100000];
+using namespace std;
+
+int A[100000];
+int B[100000];
 
 int main() {
     fstream fin, fout;
@@ -10,12 +12,37 @@ int main() {
     int n;
     fin >> n;
     for (int i = 0; i < n; i++) {
-        fin >> A[i].first >> A[i].second;
+        fin >> A[i] >> B[i];
     }
     fin.close();
 
     sort(A, A+n);
+    sort(B, B+n);
 
+    int closing = 0, opening = 0;
+    int thisClosing, thisOpening;
+    int level = 1, i = 0, j = 0;
+    while (i < n && j < n) {
+        if (i < n-1 && B[j] < A[i+1]) {
+            level--;
+            j++;
+        } else {
+            i++;
+            level++;
+        }
+        if (level == 0) {
+            thisClosing = B[j-1];
+            thisOpening = A[i+1];
+        }
+        if ((thisOpening-thisClosing) > (opening-closing)) {
+            closing = thisClosing;
+            opening = thisOpening;
+        }
+    }
 
+    fout.open("output.txt", ios::out);
+    fout << closing << " " << opening << endl;
+    fout.close();
+
+    return 0;
 }
-
