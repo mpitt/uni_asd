bf9ef245a4a148282f096472ca0d63ea132e9bd4
earendil02 <earendil002@gmail.com>
2013-04-04 21:32:27 +0200

    Tarjan algorithm implementation

--- lab04/componente/componente.cpp
+++ lab04/componente/componente.cpp
@@ -1,56 +1,83 @@
 #include <fstream>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
 struct node {
     vector <int> adj;
-    int start;
-    int end;
+    int index;
+    int lowLink;
+    bool inStack;
 };
 
-void dfs(node graph[], node v, int &time);
+void tarjan(node graph[], int v, int *index, stack <int> *S,  int *dim);
+int min(int a, int b);
 
 int main() {
     fstream file;
-    int n, m, dim=0;
-    queue <int> Q;
+    int n, m, dim=0, index=1;
+    stack <int> S;
+//Read the graph
     file.open("input.txt", ios::in);
     file >> n >> m;
     node* graph = new node[n];
-    node* graphT = new node[n];
     int x, y;
     for (int i=0; i<m; i++) {
         file >> x >> y;
         graph[x].adj.push_back(y);
-        graphT[y].adj.push_back(x);
     }
     file.close();
-    
+//Initialize the graph
     for (int i=0; i<n; i++) {
-        graph[i].start=0;
-        graph[i].end=0;
-        graphT[i].start=0;
-        graphT[i].end=0;
+        graph[i].index=0;
+        graph[i].lowLink=0;
+        graph[i].inStack=false;
     }
-
-
-
+//Compute the dimension of the largest strongly connected component of the graph
+//Use tarjan algorithm
+    for (int i=0; i<n; i++) {
+        if (graph[i].index==0) {
+            tarjan(graph, i, &index, &S, &dim);
+        }
+    }
+//Write the result
     file.open("output.txt", ios::out);
     file << dim  << endl;
     file.close();
     return 0;
 }
 
-void dfs(node graph[], int v, int &time) {
-    time++;
-    graph[v].start=++time;
+void tarjan(node graph[], int v, int *index, stack <int> *S,  int *dim) {
+    graph[v].index = *index;
+    graph[v].lowLink = *index;
+    (*index)++;
+    (*S).push(v);
+    graph[v].inStack = true;
+
     for (int i=0; i<graph[v].adj.size(); i++) {
         int w=graph[v].adj[i];
-        if(graph[w].start==0) {
-            dfs(graph, w, &time);
+        if(graph[w].index==0) {
+            tarjan(graph, w, index, S, dim);
+            graph[v].lowLink = min(graph[v].lowLink, graph[w].lowLink);
         }
-        time++;
-        graph[v].end=time;
+        else if (graph[w].inStack) {
+            graph[v].lowLink = min(graph[v].lowLink, graph[w].index);
+        }
     }
+    if (graph[v].index == graph[v].lowLink) {
+        for (int count=1, x=-1; x!=v; count++) {
+            x=(*S).top();
+            (*S).pop();
+            graph[x].inStack=false;
+            (*dim)=-min(-(*dim),-count); //equivalent to (*dim)=max((*dim),count)
+        }
+    }
+}
+
+int min(int a, int b) {
+    if (a<b) {
+        return a;
+    }
+    return b;
 }
