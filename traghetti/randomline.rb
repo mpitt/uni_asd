25ec673518cd94d71b0308dc4090a936032ed411
Jules <michelon.giulio@gmail.com>
2013-04-10 23:41:11 +0200

    [traghetti] Script to generate lines

---
+++ traghetti/randomline.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+if ARGV.length != 1 then
+    puts "Usage: randomline.rb <num_nodes>"
+    exit 1
+end
+
+n = ARGV[0].to_i
+puts "#{n} #{n-1}"
+a = (1..n).to_a.shuffle
+a.each_index do |i|
+    if a[i+1] then
+        puts "#{a[i]} #{a[i+1]}"
+    end
+end
+
+exit 0
