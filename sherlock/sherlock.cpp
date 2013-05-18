a464335dd30165b636a9fafc1133b83f2d616007
Riccardo Longo <earendil002@gmail.com>
2013-05-18 19:43:01 +0200

    Finish iterated version, 20 correct 0 timeout

--- sherlock/sherlock.cpp
+++ sherlock/sherlock.cpp
@@ -62,18 +62,18 @@ int main() {
     P[i] = new int*[travestimenti + 2];
     for (int j = 0; j < travestimenti + 2; j++) {
       P[i][j] = new int[2];
+      P[i][j][0] = P[i][j][1] = 0; 
     }
   }
-
   /* FORMATO OUTPUT
    * un numero che rappresenta il massimo numero di istanti in cui
    * Sherlock può avere il travestimento adatto
    */
-  fout.open("output.txt", ios::out);
-  fout << fapunti(P, serate, istanti, total, travestimenti, chi, finali, lunghezze) << endl;
-  fout.close();
+fout.open("output.txt", ios::out);
+fout << fapunti(P, serate, istanti, total, travestimenti, chi, finali, lunghezze) << endl;
+fout.close();
 
-  return 0;
+return 0;
 }
 
 //magica funzione risolvi i nostri problemi, amen
@@ -83,46 +83,37 @@ int fapunti(int*** P, int serate, int istanti, int total, int travestimenti, lis
   bool pari = false;
   list<int>::reverse_iterator iter = chi[serata].rbegin(), next;
 
-  for (int ist = total; ist >= 0; ist--) {
-    for (int rim = 0; rim <= travestimenti + 1; rim++) {
+  bool cambiasera = false;
+  P[total][0][false] = inf;
+  P[total][0][true] = inf;
+  for (int ist = total - 1; ist >= 0; ist--) {
+    P[ist][0][false] = inf;
+    P[ist][0][true] = inf;
 
-      if (rim == 0) {
-        P[ist][rim][false] = inf;
-        P[ist][rim][true] = inf;
-
-      } else if (ist == total){
-        P[ist][rim][false] = 0;
-        P[ist][rim][true] = 0;
-
+    for (int rim = 1; rim <= travestimenti + 1; rim++) {
+      next = iter;
+      next++;
+      if (next == chi[serata].rend()) {
+        int m = P[ist + lunghezze[serata]][rim][0];
+        m = max(m, P[ist + 1][rim - 1][true] + g(*iter, true, finali, serata, pari));
+        m = max(m, P[ist + 1][rim - 1][false] + g(*iter, false, finali, serata, pari));
+        P[ist][rim][false] = m;
+        P[ist][rim][true] = m;
+        cambiasera = (serata > 0);
       } else {
-        next = iter;
-        next++;
-        if (next == chi[serata].rend()) {
-//          cout << "inizio sera\n";
-          int m = P[ist + lunghezze[serata]][rim][0];
-          m = max(m, P[ist + 1][rim - 1][true] + g(*iter, true, finali, serata, pari));
-          m = max(m, P[ist + 1][rim - 1][false] + g(*iter, false, finali, serata, pari));
-          P[ist][rim][false] = m;
-          P[ist][rim][true] = m;
-//          cout << "in ist: " << ist << ", rim: " << rim - 1 << ", scelgo te! -> " << m << endl;
-
-          if (serata > 0) {
-            serata--;
-            iter = chi[serata].rbegin();
-            pari = false;
-          }
-        } else {
-//          cout <<"proseguio\n";
-          P[ist][rim][true] = max(P[ist + 1][rim][true] + g(*iter, true, finali, serata, pari),
-              P[ist + 1][rim - 1][false] + g(*iter, false, finali, serata, pari));
-          P[ist][rim][false] = max(P[ist + 1][rim][false] + g(*iter, false, finali, serata, pari),
-              P[ist + 1][rim - 1][true] + g(*iter, true, finali, serata, pari));
-//          cout << "in ist: " << ist << ", rim: " << rim - 1 << ", 0  -> " << P[ist][rim][0] << endl;
-//          cout << "in ist: " << ist << ", rim: " << rim - 1 << ", 1  -> " << P[ist][rim][1] << endl;
-          ++iter;
-          pari = !pari;
-        } 
-      }
+        P[ist][rim][true] = max(P[ist + 1][rim][true] + g(*iter, true, finali, serata, pari),
+            P[ist + 1][rim - 1][false] + g(*iter, false, finali, serata, pari));
+        P[ist][rim][false] = max(P[ist + 1][rim][false] + g(*iter, false, finali, serata, pari),
+            P[ist + 1][rim - 1][true] + g(*iter, true, finali, serata, pari));
+      } 
+    }
+    if (cambiasera) {
+      serata--;
+      iter = chi[serata].rbegin();
+      pari = cambiasera = false;
+    } else {
+      ++iter;
+      pari = !pari;
     }
   }
   return P[0][travestimenti + 1][false];
