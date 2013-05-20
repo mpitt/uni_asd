2ac46f582765d5ee844e2975041639ddd45253be
Riccardo Longo <earendil002@gmail.com>
2013-05-20 04:39:21 +0200

    Finish double DP version, but still 3 sig6 (11,18,19)

--- sherlock/sherlock.cpp
+++ sherlock/sherlock.cpp
@@ -4,8 +4,9 @@
 
 using namespace std;
 
-int fapunti(int*** P, int serate, int istanti, int total, int travestimenti, list<int>* chi, int* finali, int* lunghezze);
+int fapunti(int** P, int serate, int travestimenti, int** G, int* lunghezze);
 int g(int iter, bool trav, int* finali, int serata, bool pari);
+void analizza_sera(int serata, int lun, int finale, int** G, list<int> chi, int inf);
 
 int main() {
   //variabili
@@ -56,73 +57,91 @@ int main() {
   fin.close();
 
   //inizializzo la tabella dei punti
-  int ***P = new int**[total + 1];
-
+  int ***S = new int**[total + 1];
   for (int i = 0; i < total + 1; i++) {
-    P[i] = new int*[travestimenti + 2];
+    S[i] = new int*[travestimenti + 2];
     for (int j = 0; j < travestimenti + 2; j++) {
-      P[i][j] = new int[2];
-      P[i][j][0] = P[i][j][1] = 0; 
+      S[i][j] = new int[2];
+      S[i][j][0] = S[i][j][1] = 0; 
     }
+  }
+  int** G = new int*[serate];
+  for (int i = 0; i < serate; i++) {
+    G[i] = new int[travestimenti];
+    for (int j = 0; j < travestimenti; j++) {
+      G[i][j] = 0;
+    }
+  }
+  for (int i = 0; i < serate; i++) {
+    analizza_sera(i, lunghezze[i], finali[i], G, chi[i], -bottom);
+  }
+  int** P = new int*[serate + 1];
+  for (int i = 0; i <= serate; i++) {
+    P[i] = new int[travestimenti + 2];
+    P[i][0] = -bottom;
+  }
+  for (int i = 1; i < travestimenti + 2; i++) {
+    P[serate][i] = 0;
   }
   /* FORMATO OUTPUT
    * un numero che rappresenta il massimo numero di istanti in cui
    * Sherlock può avere il travestimento adatto
    */
 fout.open("output.txt", ios::out);
-fout << fapunti(P, serate, istanti, total, travestimenti, chi, finali, lunghezze) << endl;
+fout << fapunti(P, serate, travestimenti, G, lunghezze) << endl;
 fout.close();
 
 return 0;
 }
 
 //magica funzione risolvi i nostri problemi, amen
-int fapunti(int*** P, int serate, int istanti, int total, int travestimenti, list<int>* chi, int* finali, int* lunghezze) {
-  int inf = -(serate * istanti + 1);
-  int serata = serate - 1;
-  bool pari = false;
-  list<int>::reverse_iterator iter = chi[serata].rbegin(), next;
-
-  bool cambiasera = false;
-  P[total][0][false] = inf;
-  P[total][0][true] = inf;
-  for (int ist = total - 1; ist >= 0; ist--) {
-    P[ist][0][false] = inf;
-    P[ist][0][true] = inf;
-
+int fapunti(int** P, int serate, int travestimenti, int** G, int* lunghezze) {
+  for (int s = serate - 1; s >= 0; s--) {
     for (int rim = 1; rim <= travestimenti + 1; rim++) {
-      next = iter;
-      next++;
-      if (next == chi[serata].rend()) {
-        int m = P[ist + lunghezze[serata]][rim][0];
-        m = max(m, P[ist + 1][rim - 1][true] + g(*iter, true, finali, serata, pari));
-        m = max(m, P[ist + 1][rim - 1][false] + g(*iter, false, finali, serata, pari));
-        P[ist][rim][false] = m;
-        P[ist][rim][true] = m;
-        cambiasera = (serata > 0);
-      } else {
-        P[ist][rim][true] = max(P[ist + 1][rim][true] + g(*iter, true, finali, serata, pari),
-            P[ist + 1][rim - 1][false] + g(*iter, false, finali, serata, pari));
-        P[ist][rim][false] = max(P[ist + 1][rim][false] + g(*iter, false, finali, serata, pari),
-            P[ist + 1][rim - 1][true] + g(*iter, true, finali, serata, pari));
-      } 
-    }
-    if (cambiasera) {
-      serata--;
-      iter = chi[serata].rbegin();
-      pari = cambiasera = false;
-    } else {
-      ++iter;
-      pari = !pari;
+      int m = P[s +1][rim];
+      for (int k = 1; k < rim && k <=  lunghezze[s]; k++) {
+        m = max(m, P[s + 1][rim - k] + G[s][k - 1]);
+      }
+      P[s][rim] = m;
     }
   }
-  return P[0][travestimenti + 1][false];
+  return P[0][travestimenti + 1];
 }
 
-int g(int points, bool trav, int* finali, int serata, bool pari) {
-  if ((pari != finali[serata]) == trav) {
+int g(int points, bool trav, int finale, bool pari) {
+  if ((pari != finale) == trav) {              // NB: != corrisponde a XOR
     return points;
   } else {
     return 0;
+  }
+}
+
+void analizza_sera(int serata, int lun, int finale, int** G, list<int> chi, int inf) {
+  int*** S = new int**[lun + 1];
+  for (int i = 0; i <= lun; i++) {
+    S[i] = new int*[lun + 2];
+    for (int j = 0; j < lun + 2; j++) {
+      S[i][j] = new int[2];
+      S[i][j][0] = S[i][j][1] = 0; 
+    }
+    S[i][0][0] = S[i][0][1] = inf;
+  }
+  bool pari = false;
+  list<int>::reverse_iterator iter = chi.rbegin();
+  for (int ist = lun - 1; ist > 0; ist--) {
+    for (int rim = 1; rim <= lun + 1; rim++) {
+      S[ist][rim][true] = max(S[ist + 1][rim][true] + g(*iter, true, finale, pari),
+          S[ist + 1][rim - 1][false] + g(*iter, false, finale, pari));
+      S[ist][rim][false] = max(S[ist + 1][rim][false] + g(*iter, false, finale, pari),
+          S[ist + 1][rim - 1][true] + g(*iter, true, finale, pari));
+    } 
+    iter++;
+    pari = !pari;
+  } 
+  for (int rim = 1; rim <= lun; rim++) {
+    int m = 0;
+    m = max(m, S[1][rim][true] + g(*iter, true, finale, pari));
+    m = max(m, S[1][rim][false] + g(*iter, false, finale, pari));
+    G[serata][rim - 1] = m;
   }
 }
